13956257c7211063dff3cc1c414de048ff1b27c1
Daniel Lee <46286577+dlee07@users.noreply.github.com>
2019-12-11 14:58:24 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -14,12 +14,48 @@ const $ = cheerio.load(dom);
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://dlee07:comp20@scores-qpnhf.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
+
+var router = express.Router();
+var url ='mongodb://localhost:27017/scores';
+const port = 3000 || process.env.PORT;
+app.listen(port, function(){
+	console.log('Listening on port ' + port); //Listening on port 8888
+})
+
+
+app.get('/', function(req, res, next){
+		res.sendFile(__dirname + '/game.html');
+});
+       
+
+       			
+// router.post('/insert', function(req, res, next){
+//     client.connect(err=> {
+//         const collection = client.db("scores").collection("game");
+//        	var doc = {
+//        							    name: req.body.player_name,
+//        							    score: req.body.score
+//        	}
+
+//        	collection.insertOne(doc, function(err, result){
+//        	//assert.equal(null, err);
+//  				var listener = app.listen(port, function(){
+//     					console.log('Listening on port ' + listener.address().port); //Listening on port 8888
+// 							});
+//  							console.log('item inserted');
+//        	});
+       			
+//        	client.close();
+//     });
+// })
+
+/*client.connect(err => {
 	console.log("Connected");
 	const collection = client.db("scores").collection("game");
 	var users = [];
 	collection.find({}).toArray(function(err, result) {
 		users = result;
+		console.log(result);
 		console.log(users);
 		users.sort(function (a, b) {
 			return b.score - a.score;
@@ -37,13 +73,9 @@ client.connect(err => {
 		http.createServer(handleRequest).listen('./leaderboard.html');
 	}); 
     client.close();
-});
+});*/
 
-
-
-
-
-
+module.exports = router;
 
 
 
